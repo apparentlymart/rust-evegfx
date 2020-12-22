4f5625f898b4a3eb3009ac3ca54d2fa2e9c983ec
Martin Atkins <mart@degeneration.co.uk>
2020-12-21 22:42:55 -0800

    evegfx: Further work on the display_list API

--- evegfx/src/display_list.rs
+++ evegfx/src/display_list.rs
@@ -1,3 +1,6 @@
+use core::convert::TryFrom;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+
 /// Represents an EVE display list command.
 #[derive(Copy, Clone, PartialEq)]
 pub struct DLCmd(u32);
@@ -21,6 +24,81 @@ impl DLCmd {
     pub const fn begin(prim: GraphicsPrimitive) -> Self {
         OpCode::BEGIN.build(prim as u32)
     }
+
+    pub const fn bitmap_ext_format(format: BitmapExtFormat) -> Self {
+        OpCode::BITMAP_EXT_FORMAT.build(format as u32)
+    }
+
+    pub const fn bitmap_handle(bmp: BitmapHandle) -> Self {
+        OpCode::BITMAP_HANDLE.build(bmp.0 as u32)
+    }
+
+    pub const fn bitmap_layout(format: BitmapFormat, line_stride: u16, height: u16) -> Self {
+        OpCode::BITMAP_LAYOUT.build(
+            (format as u32) << 19
+                | (line_stride as u32 & 0b1111111111) << 9
+                | (height as u32 & 0b111111111),
+        )
+    }
+
+    pub const fn bitmap_layout_h(format: BitmapFormat, line_stride: u16, height: u16) -> Self {
+        OpCode::BITMAP_LAYOUT_H
+            .build((format as u32) << 19 | (line_stride as u32 >> 10) << 2 | (height as u32 >> 10))
+    }
+
+    /// `bitmap_layout_pair` is a helper for calling both `bitmap_layout` and
+    /// `bitmap_layout_h` with the same values, in order to set all 12 of
+    /// the bits in the `line_stride` and `height` fields. Write the two
+    /// commands to consecutive positions in the display list.
+    pub const fn bitmap_layout_pair(
+        format: BitmapFormat,
+        line_stride: u16,
+        height: u16,
+    ) -> (Self, Self) {
+        (
+            Self::bitmap_layout(format, line_stride, height),
+            Self::bitmap_layout_h(format, line_stride, height),
+        )
+    }
+
+    pub const fn bitmap_size(
+        width: u16,
+        height: u16,
+        filter: BitmapSizeFilter,
+        wrap_x: BitmapWrapMode,
+        wrap_y: BitmapWrapMode,
+    ) -> Self {
+        OpCode::BITMAP_SIZE.build(
+            (filter as u32) << 20
+                | (wrap_x as u32) << 19
+                | (wrap_y as u32) << 18
+                | (width as u32 & 0b111111111) << 9
+                | (height as u32 & 0b111111111),
+        )
+    }
+
+    pub const fn bitmap_size_h(width: u16, height: u16) -> Self {
+        let width = ((width as u32) >> 9) & 0b11;
+        let height = ((height as u32) >> 9) & 0b11;
+        OpCode::BITMAP_SIZE_H.build(width << 9 | height)
+    }
+
+    /// `bitmap_size_pair` is a helper for calling both `bitmap_size` and
+    /// `bitmap_size_h` with the same values, in order to set all 13 of
+    /// the bits in the `width` and `height` fields. Write the two
+    /// commands to consecutive positions in the display list.
+    pub const fn bitmap_size_pair(
+        width: u16,
+        height: u16,
+        filter: BitmapSizeFilter,
+        wrap_x: BitmapWrapMode,
+        wrap_y: BitmapWrapMode,
+    ) -> (Self, Self) {
+        (
+            Self::bitmap_size(width, height, filter, wrap_x, wrap_y),
+            Self::bitmap_size_h(width, height),
+        )
+    }
 }
 
 /// Each command is encoded as a four-byte value. Converting to `u32` returns
@@ -32,14 +110,18 @@ impl Into<u32> for DLCmd {
     }
 }
 
+#[derive(Clone, Copy, PartialEq)]
 #[repr(u8)]
+#[allow(non_camel_case_types)]
 enum OpCode {
     ALPHA_FUNC = 0x09,
     BEGIN = 0x1F,
+    BITMAP_EXT_FORMAT = 0x2e,
     BITMAP_HANDLE = 0x05,
     BITMAP_LAYOUT = 0x07,
     BITMAP_LAYOUT_H = 0x28,
     BITMAP_SIZE = 0x08,
+    BITMAP_SIZE_H = 0x29,
 }
 
 impl OpCode {
@@ -52,6 +134,7 @@ impl OpCode {
     }
 }
 
+#[derive(TryFromPrimitive, IntoPrimitive, Clone, Copy, PartialEq)]
 #[repr(u8)]
 pub enum GraphicsPrimitive {
     Bitmaps = 1,
@@ -65,6 +148,7 @@ pub enum GraphicsPrimitive {
     Rects = 9,
 }
 
+#[derive(TryFromPrimitive, IntoPrimitive, Clone, Copy, PartialEq)]
 #[repr(u8)]
 pub enum AlphaTestFunc {
     Never = 0,
@@ -75,4 +159,167 @@ pub enum AlphaTestFunc {
     Equal = 5,
     NotEqual = 6,
     Always = 7,
+}
+
+#[derive(TryFromPrimitive, IntoPrimitive, Clone, Copy, PartialEq)]
+#[repr(u16)]
+pub enum BitmapExtFormat {
+    ARGB1555 = 0,
+    L1 = 1,
+    L4 = 2,
+    L8 = 3,
+    RGB332 = 4,
+    ARGB2 = 5,
+    ARGB4 = 6,
+    RGB565 = 7,
+    Text8x8 = 9,
+    TextVGA = 10,
+    Bargraph = 11,
+    Paletted565 = 14,
+    Paletted4444 = 15,
+    Paletted8 = 16,
+    L2 = 17,
+    CompressedRGBAASTC4x4KHR = 37808,
+    CompressedRGBAASTC5x4KHR = 37809,
+    CompressedRGBAASTC5x5KHR = 37810,
+    CompressedRGBAASTC6x5KHR = 37811,
+    CompressedRGBAASTC6x6KHR = 37812,
+    CompressedRGBAASTC8x5KHR = 37813,
+    CompressedRGBAASTC8x6KHR = 37814,
+    CompressedRGBAASTC8x8KHR = 37815,
+    CompressedRGBAASTC10x5KHR = 37816,
+    CompressedRGBAASTC10x6KHR = 37817,
+    CompressedRGBAASTC10x8KHR = 37818,
+    CompressedRGBAASTC10x10KHR = 37819,
+    CompressedRGBAASTC12x10KHR = 37820,
+    CompressedRGBAASTC12x12KHR = 37821,
+}
+
+#[derive(TryFromPrimitive, IntoPrimitive, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum BitmapFormat {
+    ARGB1555 = 0,
+    L1 = 1,
+    L4 = 2,
+    L8 = 3,
+    RGB332 = 4,
+    ARGB2 = 5,
+    ARGB4 = 6,
+    RGB565 = 7,
+    Text8x8 = 9,
+    TextVGA = 10,
+    Bargraph = 11,
+    Paletted565 = 14,
+    Paletted4444 = 15,
+    Paletted8 = 16,
+    L2 = 17,
+    GLFormat = 31,
+}
+
+impl TryFrom<BitmapExtFormat> for BitmapFormat {
+    type Error = ();
+    fn try_from(ext: BitmapExtFormat) -> core::result::Result<Self, ()> {
+        let raw = ext as u16;
+        if raw > 17 {
+            return Err(());
+        }
+        match BitmapFormat::try_from(raw as u8) {
+            Ok(v) => Ok(v),
+            Err(_) => Err(()),
+        }
+    }
+}
+
+impl TryFrom<BitmapFormat> for BitmapExtFormat {
+    type Error = ();
+    fn try_from(fmt: BitmapFormat) -> core::result::Result<Self, ()> {
+        let raw = fmt as u8;
+        if raw > 7 {
+            // The first 17 formats are common, but the others are not
+            return Err(());
+        }
+        match BitmapExtFormat::try_from(raw as u16) {
+            Ok(v) => Ok(v),
+            Err(_) => Err(()),
+        }
+    }
+}
+
+/// `BitmapHandle` is a display list bitmap handle, numbered between zero and
+/// 31.
+#[derive(Copy, Clone, PartialEq)]
+pub struct BitmapHandle(u8);
+
+impl BitmapHandle {
+    //// Mask representing the bits of a u8 that contribute to an EVEAddress.
+    pub const MASK: u8 = 0x1f;
+
+    /// `SCRATCH` is the bitmap handle reserved for use by some coprocessor
+    /// behaviors. If you aren't using the coprocessor then there's nothing
+    /// special about this handle.
+    pub const SCRATCH: Self = Self::force_raw(15);
+
+    /// Test whether the given raw value is within the expected
+    /// range for a bitmap handle, returning `true` only if so.
+    pub const fn is_valid(raw: u8) -> bool {
+        // Only the lowest 22 bits may be nonzero.
+        (raw & Self::MASK) == 0
+    }
+
+    /// Turns the given raw value into a valid BitmapHandle by masking
+    /// out the bits that must always be zero for a valid handle.
+    ///
+    /// This is intended primarily for initializing global constants
+    /// representing well-known bitmap handles in your program. If you're
+    /// working with a dynamically-derived address value then better to use the
+    /// `TryFrom<u8>` implementation to get an error if the value is out of
+    /// range.
+    pub const fn force_raw(raw: u8) -> Self {
+        Self(raw & Self::MASK)
+    }
+
+    /// Returns `true` if the handle is one of the ones that has a preassigned
+    /// special purpose. These special purposes are optional but you may wish
+    /// to prefer using non-special handles if any are available.
+    pub const fn is_special(self) -> bool {
+        self.0 >= 15
+    }
+}
+
+impl TryFrom<u8> for BitmapHandle {
+    type Error = ();
+
+    fn try_from(raw: u8) -> Result<Self, Self::Error> {
+        if Self::is_valid(raw) {
+            Ok(Self(raw))
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl From<BitmapHandle> for u8 {
+    fn from(bmp: BitmapHandle) -> u8 {
+        bmp.0
+    }
+}
+
+impl From<BitmapHandle> for u32 {
+    fn from(bmp: BitmapHandle) -> u32 {
+        bmp.0 as u32
+    }
+}
+
+#[derive(TryFromPrimitive, IntoPrimitive, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum BitmapSizeFilter {
+    Nearest = 0,
+    Bilinear = 1,
+}
+
+#[derive(TryFromPrimitive, IntoPrimitive, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum BitmapWrapMode {
+    Border = 0,
+    Repeat = 1,
 }
