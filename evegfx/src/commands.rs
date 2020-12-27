d9be863ca97b56f34bdb9e88ceb9052312d87895
Martin Atkins <mart@degeneration.co.uk>
2020-12-27 10:44:53 -0800

    evegfx: More tests for the coprocessor object
    Now has its own specialized MockInterface because that way we can check
    that it's following the expected invariants in its behavior and we can
    produce a more tailored call log that focuses only on what's interesting
    for testing the coprocessor interface.

--- evegfx/src/commands.rs
+++ evegfx/src/commands.rs
@@ -418,9 +418,10 @@ mod tests {
     extern crate std;
 
     use super::*;
-    use crate::interface::testing::{MockInterface, MockInterfaceCall};
+    use crate::interface::{EVEAddress, EVECommand};
     use crate::registers::EVERegister::*;
     use std::vec;
+    use std::vec::Vec;
 
     fn test_obj<F: FnOnce(&mut MockInterface)>(
         setup: F,
@@ -428,46 +429,345 @@ mod tests {
     {
         let mut interface = MockInterface::new();
         setup(&mut interface);
-        match EVECoprocessor::new_polling(interface) {
+        unwrap_copro(EVECoprocessor::new_polling(interface))
+    }
+
+    fn unwrap_copro<R, W: EVECoprocessorWaiter<MockInterface>>(
+        v: Result<R, EVECoprocessorError<EVECoprocessor<MockInterface, W>>>,
+    ) -> R {
+        match v {
             Ok(v) => v,
-            Err(_) => panic!("failed to construct test object"),
+            Err(err) => match err {
+                EVECoprocessorError::Interface(err) => {
+                    std::panic!("interface error: {:?}", err);
+                }
+                EVECoprocessorError::Waiter(_) => {
+                    std::panic!("waiter error");
+                }
+            },
         }
     }
 
-    fn assert_success<R, E>(v: Result<R, E>) -> R {
-        match v {
-            Ok(v) => v,
-            Err(_) => panic!("call failed"),
-        }
+    #[test]
+    fn test_handle_buffer_space() {
+        let mut cp = test_obj(|ei| {
+            // For this test we'll make the available buffer space much
+            // smaller than normal, so we can see the coprocessor object
+            // handle what looks like running out of buffer space.
+            ei.current_space = 16;
+        });
+
+        // Some junk data just to use up buffer space.
+        unwrap_copro(cp.append_raw_word(0xf4ce0001));
+        unwrap_copro(cp.append_raw_word(0xf4ce0002));
+        unwrap_copro(cp.append_raw_word(0xf4ce0003));
+        // We now have only four bytes left, but the following command needs
+        // eight so we should ReadSpace before writing it.
+        unwrap_copro(cp.wait_microseconds(127));
+
+        let ei = unwrap_copro(cp.take_interface());
+        let got = ei.calls();
+        let want = vec![
+            MockInterfaceCall::ReadSpace(16),
+            MockInterfaceCall::StartStream,
+            MockInterfaceCall::Write(0xf4ce0001), // Junk data 1
+            MockInterfaceCall::Write(0xf4ce0002), // Junk data 2
+            MockInterfaceCall::Write(0xf4ce0003), // Junk data 3
+            // The coprocessor object now only knows about four remaining
+            // bytes of buffer space, but the next command requires eight and
+            // so we'll poll to see if more space is available before
+            // continuing.
+            MockInterfaceCall::StopStream,
+            MockInterfaceCall::ReadSpace(16),
+            MockInterfaceCall::StartStream,
+            // There is now >= 8 bytes buffer space, so we can continue with
+            // appending the two words of the wait_microseconds command.
+            MockInterfaceCall::Write(0xffffff65), // CMD_WAIT
+            MockInterfaceCall::Write(127),        // the duration value from above
+            MockInterfaceCall::StopStream,
+        ];
+        debug_assert_eq!(&got[..], &want[..]);
     }
 
     #[test]
     fn test_new_display_list() {
         let mut cp = test_obj(|_| {});
 
-        assert_success(cp.new_display_list(|cp| cp.append_raw_word(0xdeadbeef)));
+        unwrap_copro(cp.new_display_list(|cp| {
+            // We must import this trait in order to call the display list building
+            // methods on the coprocessor object.
+            use crate::display_list::EVEDisplayListBuilder;
 
-        let ei = assert_success(cp.take_interface());
+            cp.append_raw_word(0xdeadbeef)?;
+            cp.clear_color_alpha(127)
+        }));
+
+        let ei = unwrap_copro(cp.take_interface());
         let got = ei.calls();
         let want = vec![
-            // Initial reset
-            MockInterfaceCall::BeginWrite(CPURESET.address()),
-            MockInterfaceCall::ContinueWrite(vec![0x01 as u8]),
-            MockInterfaceCall::EndWrite(CPURESET.address()),
-            MockInterfaceCall::BeginWrite(CPURESET.address()),
-            MockInterfaceCall::ContinueWrite(vec![0x00 as u8]),
-            MockInterfaceCall::EndWrite(CPURESET.address()),
-            // Initial synchronize
-            MockInterfaceCall::BeginRead(CMDB_SPACE.address()),
-            MockInterfaceCall::ContinueRead(2),
-            MockInterfaceCall::EndRead(CMDB_SPACE.address()),
-            // The new_display_list call
-            MockInterfaceCall::BeginWrite(CMDB_WRITE.address()),
-            MockInterfaceCall::ContinueWrite(vec![0x00, 0xff, 0xff, 0xff as u8]),
-            MockInterfaceCall::ContinueWrite(vec![239, 190, 173, 222 as u8]),
-            MockInterfaceCall::ContinueWrite(vec![0x01, 0xff, 0xff, 0xff as u8]),
-            MockInterfaceCall::EndWrite(CMDB_WRITE.address()),
+            MockInterfaceCall::ReadSpace(4092),
+            MockInterfaceCall::StartStream,
+            MockInterfaceCall::Write(0xffffff00), // CMD_DLSTART
+            MockInterfaceCall::Write(0xdeadbeef), // Fake display list command
+            MockInterfaceCall::Write(0x0f00007f), // CLEAR_COLOR_A(127)
+            MockInterfaceCall::Write(0xffffff01), // CMD_SWAP
+            MockInterfaceCall::StopStream,
         ];
         debug_assert_eq!(&got[..], &want[..]);
+    }
+
+    #[test]
+    fn test_show_testcard() {
+        let mut cp = test_obj(|_| {});
+
+        unwrap_copro(cp.show_testcard());
+
+        let ei = unwrap_copro(cp.take_interface());
+        let got = ei.calls();
+        let want = vec![
+            MockInterfaceCall::ReadSpace(4092),
+            MockInterfaceCall::StartStream,
+            MockInterfaceCall::Write(0xffffff61), // CMD_TESTCARD
+            MockInterfaceCall::StopStream,
+        ];
+        debug_assert_eq!(&got[..], &want[..]);
+    }
+
+    #[test]
+    fn test_show_manufacturer_logo() {
+        let mut cp = test_obj(|_| {});
+
+        unwrap_copro(cp.show_manufacturer_logo());
+
+        let ei = unwrap_copro(cp.take_interface());
+        let got = ei.calls();
+        let want = vec![
+            MockInterfaceCall::ReadSpace(4092),
+            MockInterfaceCall::StartStream,
+            MockInterfaceCall::Write(0xffffff31), // CMD_LOGO
+            MockInterfaceCall::StopStream,
+        ];
+        debug_assert_eq!(&got[..], &want[..]);
+    }
+
+    #[test]
+    fn test_wait_microseconds() {
+        let mut cp = test_obj(|_| {});
+
+        unwrap_copro(cp.wait_microseconds(12345));
+
+        let ei = unwrap_copro(cp.take_interface());
+        let got = ei.calls();
+        let want = vec![
+            MockInterfaceCall::ReadSpace(4092),
+            MockInterfaceCall::StartStream,
+            MockInterfaceCall::Write(0xffffff65), // CMD_WAIT
+            MockInterfaceCall::Write(12345),      // the duration value from above
+            MockInterfaceCall::StopStream,
+        ];
+        debug_assert_eq!(&got[..], &want[..]);
+    }
+
+    /// A test double for `trait Interface`, available only in test mode.
+    pub struct MockInterface {
+        write_addr: Option<EVEAddress>,
+        read_addr: Option<EVEAddress>,
+
+        pub(crate) current_space: u16,
+
+        // calls_ is the call log. Each call to a mock method appends one
+        // entry to this vector, including any that fail.
+        calls_: Vec<MockInterfaceCall>,
+    }
+
+    #[derive(Clone)]
+    pub enum MockInterfaceCall {
+        ReadSpace(u16),
+        Write(u32),
+        StartStream,
+        StopStream,
+    }
+
+    impl std::fmt::Debug for MockInterfaceCall {
+        fn fmt(
+            &self,
+            f: &mut core::fmt::Formatter<'_>,
+        ) -> core::result::Result<(), core::fmt::Error> {
+            match self {
+                MockInterfaceCall::ReadSpace(space) => write!(f, "ReadSpace({:#4?})", space),
+                MockInterfaceCall::Write(v) => write!(f, "Write({:#010x?})", v),
+                MockInterfaceCall::StartStream => write!(f, "StartStream"),
+                MockInterfaceCall::StopStream => write!(f, "StopStream"),
+            }
+        }
+    }
+
+    impl MockInterface {
+        const SPACE_ADDR: EVEAddress = CMDB_SPACE.address();
+        const WRITE_ADDR: EVEAddress = CMDB_WRITE.address();
+
+        pub fn new() -> Self {
+            Self {
+                write_addr: None,
+                read_addr: None,
+                current_space: 0xffc,
+                calls_: Vec::new(),
+            }
+        }
+
+        /// Consumes the mock and returns all of the calls it logged
+        /// during its life.
+        pub fn calls(self) -> Vec<MockInterfaceCall> {
+            self.calls_
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct MockError(&'static str);
+
+    impl EVEInterface for MockInterface {
+        type Error = MockError;
+
+        fn begin_write(&mut self, addr: EVEAddress) -> core::result::Result<(), Self::Error> {
+            if let Some(_) = self.write_addr {
+                return Err(MockError("begin_write when a write is already active"));
+            }
+            if let Some(_) = self.read_addr {
+                return Err(MockError("begin_write when a read is already active"));
+            }
+            if addr == Self::WRITE_ADDR {
+                self.calls_.push(MockInterfaceCall::StartStream);
+            }
+            if addr == Self::SPACE_ADDR {
+                return Err(MockError("mustn't write to REG_CMDB_SPACE"));
+            }
+            self.write_addr = Some(addr);
+            Ok(())
+        }
+
+        fn continue_write(&mut self, buf: &[u8]) -> core::result::Result<(), Self::Error> {
+            match self.write_addr {
+                Some(addr) => {
+                    if addr == Self::WRITE_ADDR {
+                        if buf.len() != 4 {
+                            return Err(MockError("must write to REG_CMDB_WRITE using wr32"));
+                        }
+                        let v = (buf[0] as u32)
+                            | (buf[1] as u32) << 8
+                            | (buf[2] as u32) << 16
+                            | (buf[3] as u32) << 24;
+                        self.calls_.push(MockInterfaceCall::Write(v));
+                    }
+                    // We ignore all other writes because they aren't relevant
+                    // to our coprocessor testing.
+                    Ok(())
+                }
+                None => Err(MockError("continue_write without an active write")),
+            }
+        }
+
+        fn end_write(&mut self) -> core::result::Result<(), Self::Error> {
+            let result = match self.write_addr {
+                Some(addr) => {
+                    if addr == Self::WRITE_ADDR {
+                        self.calls_.push(MockInterfaceCall::StopStream);
+                    }
+                    // We ignore all other addresses because they aren't relevant
+                    // to our coprocessor testing.
+                    Ok(())
+                }
+                None => Err(MockError("end_write without an active write")),
+            };
+            self.write_addr = None;
+            result
+        }
+
+        fn begin_read(&mut self, addr: EVEAddress) -> core::result::Result<(), Self::Error> {
+            if let Some(_) = self.write_addr {
+                return Err(MockError("begin_read when a write is already active"));
+            }
+            if let Some(_) = self.read_addr {
+                return Err(MockError("begin_read when a read is already active"));
+            }
+            if addr == Self::WRITE_ADDR {
+                return Err(MockError("mustn't read from REG_CMDB_WRITE"));
+            }
+            self.read_addr = Some(addr);
+            Ok(())
+        }
+
+        fn continue_read(&mut self, into: &mut [u8]) -> core::result::Result<(), Self::Error> {
+            match self.read_addr {
+                Some(addr) => {
+                    if addr == Self::SPACE_ADDR {
+                        if into.len() != 2 {
+                            return Err(MockError("must read REG_CMDB_SPACE with rd16"));
+                        }
+                        self.calls_
+                            .push(MockInterfaceCall::ReadSpace(self.current_space));
+                        into[0] = (self.current_space & 0xff) as u8;
+                        into[1] = (self.current_space >> 8) as u8;
+                    }
+                    // We ignore all other writes because they aren't relevant
+                    // to our coprocessor testing.
+                    Ok(())
+                }
+                None => Err(MockError("continue_read without an active read")),
+            }
+        }
+
+        fn end_read(&mut self) -> core::result::Result<(), Self::Error> {
+            let result = match self.read_addr {
+                Some(_) => Ok(()),
+                None => Err(MockError("end_read without an active read")),
+            };
+            self.read_addr = None;
+            result
+        }
+
+        fn cmd(
+            &mut self,
+            _cmd: EVECommand,
+            _a0: u8,
+            _a1: u8,
+        ) -> core::result::Result<(), Self::Error> {
+            // Commands aren't relevant to our coprocessor tests.
+            Ok(())
+        }
+    }
+
+    impl PartialEq for MockInterfaceCall {
+        fn eq(&self, other: &Self) -> bool {
+            match self {
+                MockInterfaceCall::ReadSpace(self_space) => {
+                    if let MockInterfaceCall::ReadSpace(other_space) = other {
+                        *self_space == *other_space
+                    } else {
+                        false
+                    }
+                }
+                MockInterfaceCall::Write(self_data) => {
+                    if let MockInterfaceCall::Write(other_data) = other {
+                        self_data.eq(other_data)
+                    } else {
+                        false
+                    }
+                }
+                MockInterfaceCall::StartStream => {
+                    if let MockInterfaceCall::StartStream = other {
+                        true
+                    } else {
+                        false
+                    }
+                }
+                MockInterfaceCall::StopStream => {
+                    if let MockInterfaceCall::StopStream = other {
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        }
     }
 }
